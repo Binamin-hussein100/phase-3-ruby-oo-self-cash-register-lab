e1448a5f40a57e546be2057216781bfc7054ed65
binamin-hussein100 <binamin.h.hassan14@gmail.com>
2022-07-23 17:51:24 +0300

    done

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,12 @@
+class  CashRegister
+    attr_accessor :total, :discount
 
+    def initialize(discount= 20)
+        @total = 0
+        @discount = discount
+    end
+
+    def add_item(item, price,quantity = 1)
+        @total += price
+    end
+end
